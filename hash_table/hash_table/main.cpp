911ad2e728c33497362207974dfcc69ffe51cac0
jlukasie <lukasiewicz.john@gmail.com>
2013-11-02 19:22:27 -0700

    initial project commit

---
+++ hash_table/hash_table/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hash_table
+//
+//  Created by John Lukasiewicz on 11/2/13.
+//  Copyright (c) 2013 John Lukasiewicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
